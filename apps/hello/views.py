3610ecc8388d90e7437a296e928bb18cafbfe9c2
Oleksandr Okrepkyi <o.okrepkyi@gmail.com>
2016-01-06 23:04:59 +0200

    ticket:1 Added 'person_view' function to the views.py; updated urls.py with the new view function name

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,3 +1,11 @@
+from apps.hello.models import PersonModel
 from django.shortcuts import render
 
-# Create your views here.
+
+def person_view(request):
+
+    data = PersonModel.objects.get(pk=1)
+    context = {
+    	"context_data": data,
+    }
+    return render(request, 'hello/home.html', context)
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns(
     # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'apps.hello.views.home', name='home'),
+    url(r'^$', 'apps.hello.views.person_view', name='person_view'),
     url(r'^admin/', include(admin.site.urls)),
 )
