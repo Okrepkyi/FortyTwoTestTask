ea14aa082c1271582d2924dcfc509cd044755e4c
Oleksandr Okrepkyi <o.okrepkyi@gmail.com>
2016-01-06 22:41:31 +0200

    ticket:1 Created PersonModel

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class PersonModel(models.Model):
+
+	name = models.CharField(max_length=20)
+	surname = models.CharField(max_length=20)
+	date_of_birth = models.DateField()
+	bio = models.CharField(max_length=300)
+	jabber = models.CharField(max_length=20)
+	email = models.EmailField(max_length=50)
+	skype = models.CharField(max_length=50)
+	other = models.CharField(max_length=300)
