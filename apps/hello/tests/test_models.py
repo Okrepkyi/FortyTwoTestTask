5cc0460620abc1a79f06becbcdec2884e299fd3c
Oleksandr Okrepkyi <o.okrepkyi@gmail.com>
2016-01-08 18:24:21 +0200

    ticket:1 Model test added

---
+++ apps/hello/tests/test_models.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from apps.hello.models import PersonModel
+
+
+class PersonModelTest(TestCase):
+
+    def create_person_model(self, name="Ababagalamaga", 
+    						surname="Agamalagababa", 
+    						date="1987-10-22", 
+    						bio="", 
+    						jabber="",
+    						email="",
+    						skype="",
+    						other=""):
+        return PersonModel.objects.create(name=name, 
+        									surname=surname, 
+        									date=date, bio=bio, 
+        									jabber=jabber, 
+        									email=email, 
+        									skype=skype, 
+        									other=other)
+
+    def test_person_model(self):
+        instance = self.create_person_model()
+        self.assertEqual(instance.__str__(), instance.name)
